ee2efe8f3d70210f1138e8f55ba41e66a3eb9019
Matthijs <matthijs.breeman@denhaag.nl>
2025-04-08 12:22:33 +0200

    update POM and glue

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -5,11 +5,19 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import io.cucumber.junit.CucumberOptions;
+
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"starter.stepdefinitions "},
+        plugin = {"pretty", "html:target/cucumber-reports"}
+) 
+
 public class CucumberTestSuite {
 }
